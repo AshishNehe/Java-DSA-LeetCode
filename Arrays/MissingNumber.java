0108c90287f1730906356746b2e454d3f0838b6a
AshishNehe <ashishnehe1@gmail.com>
2026-07-31 18:53:31 +0530

    Add Missing Number solution using cyclic sort

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int missingNumber(int[] nums) {
+
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+
+            int correctIndex = nums[i];
+
+            if (nums[i] < n && nums[i] != nums[correctIndex]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+
+            } else {
+                i++;
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return n;
+    }
+}
